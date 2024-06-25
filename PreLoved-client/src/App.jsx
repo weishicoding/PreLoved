a41c0640353344472cabe4cd03eb35702fedf835
grace-coding <sanwind0305@gmail.com>
2024-06-25 16:21:44 +0300

    add Dockerfile

--- PreLoved-client/src/App.jsx
+++ PreLoved-client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { Login } from './pages/user/login'
+import { Login } from './pages/user/Login'
 
 function App() {
   const [count, setCount] = useState(0)
--- PreLoved-client/src/pages/user/Login.jsx
+++ PreLoved-client/src/pages/user/Login.jsx
@@ -2,5 +2,4 @@ export const Login = () => {
 	return (
 		<>Login</>
 	)
-	
 }
