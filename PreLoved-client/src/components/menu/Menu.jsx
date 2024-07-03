d50744e945694397b1caf62b8ed35bb6e2229f7a
grace-coding <sanwind0305@gmail.com>
2024-07-03 18:08:48 +0300

    fetched menu data and rendered

--- PreLoved-client/src/components/menu/Menu.jsx
+++ PreLoved-client/src/components/menu/Menu.jsx
@@ -1,11 +1,24 @@
-import React, {useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import './Menu.css'
-import {Stack} from 'react-bootstrap'
+import axios from '../../api/axios'
 
 const Menu = () => {
+  const containerRef = useRef(null)
+  const [categories, setCategories] = useState([])
+  const [categoryChildren, setCategoryChildren] = useState([])
   const [open, setOpen] = useState(false)
   const [hover, setHover] = useState(false)
-  const handlePopEnter = () => {
+
+  const handlePopEnter = id => {
+    const fetchCategoryById = async () => {
+      try {
+        const response = await axios.get(`/category/getCategoryByMenuId?categoryId=${id}`)
+        setCategoryChildren(response?.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchCategoryById()
     setOpen(true)
   }
   const handlePopLeave = () => {
@@ -13,17 +26,39 @@ const Menu = () => {
       setOpen(false)
     }
   }
-  const categories = ['WOMENS', 'MENS', 'CHILDREN']
-  const categoryItems = categories.map(category => (
-    <div key={category} onMouseEnter={() => handlePopEnter()} onMouseLeave={() => handlePopLeave()} className="me-3 py-3 on-point">
-      {category}
+
+  useEffect(() => {
+    const fetchCategory = async () => {
+      try {
+        const response = await axios.get('/category/getCategoryFirst')
+        setCategories(response?.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchCategory()
+  }, [])
+
+  const CategoryItems = categories.map(category => (
+    <div key={category.id} onMouseEnter={() => handlePopEnter(category.id)} onMouseLeave={() => handlePopLeave()} className="me-3 py-3 on-point">
+      {category.name}
     </div>
   ))
+
+  const CategoryChildrenItems = ({item}) => (
+    <div className="px-2 py-3 me-5 on-point">
+      <Item item={item}></Item>
+      {item.categoryChildren && item.categoryChildren.map(category => <Item key={`${category.id} + ${Math.random()}`} item={category}></Item>)}
+    </div>
+  )
+
+  const Item = ({item}) => <div className={item.categoryChildren ? 'category-parent-name' : 'category-child-name'}>{item.name}</div>
+
   return (
     <>
-      <div className="d-flex px-5 border-bottom">{categoryItems}</div>
-      <div className={`pop-cover ${open || hover ? 'show' : ''} op-cover py-3 px-5 `} onMouseEnter={() => open && setHover(true)} onMouseLeave={() => setHover(false)}>
-        123123
+      <div className="d-flex px-5 border-bottom category-parent-name">{CategoryItems}</div>
+      <div className={`pop-cover ${open || hover ? 'show' : ''} op-cover py-3 px-5 d-flex`} onMouseEnter={() => open && setHover(true)} onMouseLeave={() => setHover(false)}>
+        {categoryChildren && categoryChildren.map(category => <CategoryChildrenItems key={category.id} item={category}></CategoryChildrenItems>)}
       </div>
     </>
   )
