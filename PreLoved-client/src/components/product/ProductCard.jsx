020c23f5618651d9fdbca1d1882ac7636847e4e0
grace-coding <sanwind0305@gmail.com>
2024-07-06 21:49:09 +0300

    add product list to home page

--- PreLoved-client/src/components/product/ProductCard.jsx
+++ PreLoved-client/src/components/product/ProductCard.jsx
@@ -1,22 +1,22 @@
 import React, {useState} from 'react'
 import {Button, Card} from 'react-bootstrap'
-import img from '../../assets/images/clothes.png'
+// import img from '../../assets/images/clothes.png'
 import '../../styles/productCard.css'
 import {MdFavoriteBorder, MdOutlineShoppingCart} from 'react-icons/md'
 
-export const ProductCard = () => {
+export const ProductCard = ({data}) => {
   const [open, setOpen] = useState(false)
   const handleFavorite = () => {
     console.log(123)
   }
   return (
-    <Card style={{width: '13rem', border: 'none'}}>
+    <Card style={{minWidth: '13rem', border: 'none'}}>
       <div className="bg-secondary h-100">
         <div className="d-flex justify-content-between px-3 pt-2 head-div">
           <div className="font-xxs text-light fw-bold bg-thirdary p-1 rounded">NEW</div>
           <MdFavoriteBorder size={20} className="on-point" onMouseEnter={() => setOpen(true)} onMouseLeave={() => setOpen(false)} onClick={handleFavorite}></MdFavoriteBorder>
         </div>
-        <Card.Img variant="top" src={img} className="on-point pb-2" />
+        <Card.Img variant="top" src={data.url} className="on-point pb-2" />
       </div>
       {open && <div className="favorite-tip bg-primary rounded font-xs px-2 py-1">Add to favorite</div>}
       <Card.Body className="text-center px-0 py-2">
@@ -27,9 +27,9 @@ export const ProductCard = () => {
             <MdOutlineShoppingCart size={20}></MdOutlineShoppingCart>
           </button>
         </div>
-        <div className="mt-2 font-md fw-bold">Sand</div>
-        <div className="mt-1 font-sm">Blouse, size 38</div>
-        <div className="mt-1 font-md fw-bold">$79</div>
+        <div className="mt-2 font-md fw-bold">{data.name}</div>
+        <div className="mt-1 font-sm">{data.type}, size {data.size}</div>
+        <div className="mt-1 font-md fw-bold">{data.price}</div>
       </Card.Body>
     </Card>
   )
--- PreLoved-client/src/pages/Home.jsx
+++ PreLoved-client/src/pages/Home.jsx
@@ -2,8 +2,94 @@ import React from 'react'
 import img1 from '../assets/images/men.jpg'
 import '../styles/home.css'
 import {ProductCard} from '../components/product/ProductCard'
+import img from '../assets/images/clothes.png'
 
 const Home = () => {
+  const arrivalProducts = [
+    {
+      id: 1,
+      name: 'Sand',
+      type: 'Blouse',
+      size: '38',
+      price: '$79',
+      url: img
+    },
+    {
+      id: 2,
+      name: 'Marimekko',
+      type: 'Schiffon dress',
+      size: '46',
+      price: '$159',
+      url: img
+    },
+		{
+      id: 3,
+      name: 'Sand',
+      type: 'Blouse',
+      size: '38',
+      price: '$79',
+      url: img
+    },
+    {
+      id: 4,
+      name: 'Marimekko',
+      type: 'Schiffon dress',
+      size: '46',
+      price: '$159',
+      url: img
+    },
+		{
+      id: 5,
+      name: 'Sand',
+      type: 'Blouse',
+      size: '38',
+      price: '$79',
+      url: img
+    },
+    {
+      id: 6,
+      name: 'Marimekko',
+      type: 'Schiffon dress',
+      size: '46',
+      price: '$159',
+      url: img
+    },
+		{
+      id: 7,
+      name: 'Sand',
+      type: 'Blouse',
+      size: '38',
+      price: '$79',
+      url: img
+    },
+    {
+      id: 8,
+      name: 'Marimekko',
+      type: 'Schiffon dress',
+      size: '46',
+      price: '$159',
+      url: img
+    },
+		{
+      id: 9,
+      name: 'Marimekko',
+      type: 'Schiffon dress',
+      size: '46',
+      price: '$159',
+      url: img
+    },
+		{
+      id: 10,
+      name: 'Marimekko',
+      type: 'Schiffon dress',
+      size: '46',
+      price: '$159',
+      url: img
+    }
+  ]
+
+  const ProductList = ({products}) => <div className="product-list gap-5 px-5">{products && products.map(product => <ProductCard key={product.id} data={product}></ProductCard>)}</div>
+
   return (
     <>
       <div className="text-center font-xxl fw-bold py-4 mt-3">The leading Nordic online store for second hand brand apparel</div>
@@ -16,7 +102,8 @@ const Home = () => {
         </div>
       </div>
       <div>
-        <ProductCard></ProductCard>
+        <div className='ps-4 py-3 font-xl fw-bold' style={{marginTop: "-7rem"}}>New arrivals</div>
+        <ProductList products={arrivalProducts} />
       </div>
     </>
   )
