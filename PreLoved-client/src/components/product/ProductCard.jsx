d46be3ec60fe759dd646cc83804f44b0c311a0f5
grace-coding <sanwind0305@gmail.com>
2024-07-07 13:47:53 +0300

    add productCategory page

--- PreLoved-client/src/components/product/ProductCard.jsx
+++ PreLoved-client/src/components/product/ProductCard.jsx
@@ -1,6 +1,5 @@
 import React, {useState} from 'react'
 import {Button, Card} from 'react-bootstrap'
-// import img from '../../assets/images/clothes.png'
 import '../../styles/productCard.css'
 import {MdFavoriteBorder, MdOutlineShoppingCart} from 'react-icons/md'
 
@@ -14,9 +13,11 @@ export const ProductCard = ({data}) => {
       <div className="bg-secondary h-100">
         <div className="d-flex justify-content-between px-3 pt-2 head-div">
           <div className="font-xxs text-light fw-bold bg-thirdary p-1 rounded">NEW</div>
-          <MdFavoriteBorder size={20} className="on-point" onMouseEnter={() => setOpen(true)} onMouseLeave={() => setOpen(false)} onClick={handleFavorite}></MdFavoriteBorder>
+          <MdFavoriteBorder size={22} className="on-point" onMouseEnter={() => setOpen(true)} onMouseLeave={() => setOpen(false)} onClick={handleFavorite}></MdFavoriteBorder>
         </div>
-        <Card.Img variant="top" src={data.url} className="on-point pb-2" />
+        <div className="card-img-div">
+          <Card.Img variant="top" src={data.url} className="on-point pb-2 product-image" />
+        </div>
       </div>
       {open && <div className="favorite-tip bg-primary rounded font-xs px-2 py-1">Add to favorite</div>}
       <Card.Body className="text-center px-0 py-2">
@@ -28,7 +29,9 @@ export const ProductCard = ({data}) => {
           </button>
         </div>
         <div className="mt-2 font-md fw-bold">{data.name}</div>
-        <div className="mt-1 font-sm">{data.type}, size {data.size}</div>
+        <div className="mt-1 font-sm">
+          {data.type}, size {data.size}
+        </div>
         <div className="mt-1 font-md fw-bold">{data.price}</div>
       </Card.Body>
     </Card>
